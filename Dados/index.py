730c6246074271c499641810e6c00b19ad196c37
Joana Franciscon <joanadogas1@gmail.com>
2026-04-08 21:47:41 -0300

    Feat: Adicionado história, personagens e funções básicas

---
+++ Dados/index.py
@@ -0,0 +1,33 @@
+from random import randint
+from Personagens.index import Personagem
+from Historia.index import Historia
+
+
+nomeJogador = Historia()
+jogador = Personagem(nomeJogador, 50, 10, 10, 1)
+inimigo = Personagem('Inimigo Fudido', randint(45,55), randint(5,15), randint(5,15), 1)
+
+
+
+def ataqueJogador():
+    rolarAtaqueJogador = input('Deseja rolar o dado de ataque? [Sim/Não]').upper()
+    if rolarAtaqueJogador == 'SIM':
+        dado = randint(0,5)
+        ataque = jogador.ataque + dado
+        return ataque
+    else:
+        print('Foje bot noob')
+
+def atacaInimigo():
+    ataqueRodada1 = ataqueJogador()
+    dado = ataqueRodada1 - jogador.ataque
+    print(f'Valor do dado: {dado} | Valor do ataque: {jogador.ataque} | Resultado total do ataque: {ataqueRodada1}\nDefesa do inimigo: {inimigo.defesa}\nVida do inimigo antes do ataque: {inimigo.vida}')
+    if ataqueRodada1 > inimigo.defesa:
+        dano = ataqueRodada1 - inimigo.defesa
+        inimigo.vida -= dano
+        print(f'Defesa do inimigo: {inimigo.defesa}\nVida pós ataque: {inimigo.vida}')
+    else:
+        print(f'Inimigo não atacado, defesa maior que ataque\nDefesa: {inimigo.defesa}')
+
+# ataqueJogador()
+atacaInimigo()
---
+++ Personagens/index.py
@@ -0,0 +1,12 @@
+from Historia.index import Historia
+from random import randint
+
+class Personagem:
+    def __init__(self, nome, vida, ataque, defesa, lvl):
+        self.nome = nome
+        self.vida = vida
+        self.ataque = ataque
+        self.defesa = defesa
+        self.lvl = lvl
+
+
